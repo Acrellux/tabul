5caac15298353de3fc04d4629146a40e7cb2cb64
Acrellux <76964811+Acrellux@users.noreply.github.com>
2024-11-13 18:38:20 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './styles/style.css';
 import App from './App';
 
+// Display an alert message when the app loads
+window.alert("Warning: This is a browser message example!");
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
